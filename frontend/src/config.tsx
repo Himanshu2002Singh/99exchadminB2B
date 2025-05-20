6c61de9afea2f39ef882774faa3128c843e9add7
Himanshu Singh <rajputhimanshusingh2002@gmail.com>
2025-05-21 00:01:11 +0530

    url update

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
-    // baseURL: "https://api.99exch.gold/api" 
-    baseURL: "http://localhost:5000/api" 
+    baseURL: "https://api.99exch.gold/api" 
+    // baseURL: "http://localhost:5000/api" 
    };
   
 export default config;
